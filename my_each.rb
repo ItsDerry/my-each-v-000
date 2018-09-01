e852c4aace872bd84b273fe72c8a81fc386413b5
Derrick Reyes <derrickreyes@me.com>
2018-09-01 17:36:26 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,12 +1,13 @@
 def my_each(collection)
+  if collection.kind_of?(Array) 
   i = 0
 
   while i < collection.length
     yield [i]
-    collection[i]
     i = i + 1
   end
 
   collection
-
+  else
+  oops 
 end
